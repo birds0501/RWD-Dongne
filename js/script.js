5d5466ecaf01923f2972d933933da3a34a99082f
birds0501 <birds0501@naver.com>
2025-06-04 15:33:55 +0900

    더보기 작업중

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const adSlider = new Swiper(".ad-slider", {
+  pagination: {
+    el: ".ad-slider .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  loop: true,
+});
+
+const bestSlider = new Swiper(".best-slider", {
+  slidesPerView: "auto",
+  spaceBetween: 50,
+  slidesOffsetAfter: 150,
+  watchOverflow: true,
+  freeMode: false,
+});
+
+// 신간알림 더보기 버튼
+
+// 변수 선언
+const btn = document.querySelector(".plus");
+
+btn.addEventListener("click", () => {
+  const alarmContents = document.querySelector(".alarm-con");
+  const hiddenItems = document.querySelectorAll(".item.hidden");
+
+  for (let i = 0; i < 2 && i < hiddenItems.length; i++) {
+    hiddenItems[i].classList.remove("hidden");
+  }
+
+  if (container.querySelectorAll(".item.hidden").length === 0) {
+    btn.style.display = "none";
+  }
+});
