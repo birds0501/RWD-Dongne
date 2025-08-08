e253ac96d2d35986534ab0f8ae8b8f56ec71b1a4
birds0501 <birds0501@naver.com>
2025-08-09 02:48:20 +0900

    intro-alarm-01

--- js/script.js
+++ js/script.js
@@ -23,30 +23,39 @@ const bestSlider = new Swiper(".best-slider", {
 // 변수 선언
 const btn = document.querySelector(".plus");
 
-btn.addEventListener("click", () => {
-  const alarmContents = document.querySelector(".alarm-con");
-  const hiddenItems = document.querySelectorAll(".item.hidden");
+if (btn) {
+  btn.addEventListener("click", () => {
+    const alarmContents = document.querySelector(".alarm-con");
+    const hiddenItems = document.querySelectorAll(".item.hidden");
 
-  for (let i = 0; i < 2 && i < hiddenItems.length; i++) {
-    hiddenItems[i].classList.remove("hidden");
-  }
+    for (let i = 0; i < 2 && i < hiddenItems.length; i++) {
+      hiddenItems[i].classList.remove("hidden");
+    }
 
-  if (container.querySelectorAll(".item.hidden").length === 0) {
-    btn.style.display = "none";
-  }
-});
+    if (container.querySelectorAll(".item.hidden").length === 0) {
+      btn.style.display = "none";
+    }
+  });
+}
 
 // 변수 선언
 const btnAd = document.querySelector(".plus.ad");
+if (btnAd) {
+  btnAd.addEventListener("click", () => {
+    const hiddenItems = document.querySelectorAll(".ad-item.hidden");
 
-btnAd.addEventListener("click", () => {
-  const hiddenItems = document.querySelectorAll(".ad-item.hidden");
+    for (let i = 0; i < 3 && i < hiddenItems.length; i++) {
+      hiddenItems[i].classList.remove("hidden");
+    }
 
-  for (let i = 0; i < 3 && i < hiddenItems.length; i++) {
-    hiddenItems[i].classList.remove("hidden");
-  }
+    if (container.querySelectorAll(".ad-item.hidden").length === 0) {
+      btnAd.style.display = "none";
+    }
+  });
+}
 
-  if (container.querySelectorAll(".ad-item.hidden").length === 0) {
-    btnAd.style.display = "none";
-  }
+//서비스 소개-신간알림-리뷰 슬라이더
+const swiper = new Swiper(".review-wrap", {
+  slidesPerView: "auto", // 자동이 아닌, 카드 width 기준으로 보여줌
+  spaceBetween: 0,
 });
