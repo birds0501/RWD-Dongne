b059d231ca69f68999a720178078072f9273fdca
birds0501 <birds0501@naver.com>
2025-08-16 20:05:15 +0900

    동네서점지도-03

--- js/script.js
+++ js/script.js
@@ -1,26 +1,4 @@
 $(function () {
-  const adSlider = new Swiper(".ad-slider", {
-    pagination: {
-      el: ".ad-slider .swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    loop: true,
-  });
-
-  const bestSlider = new Swiper(".best-slider", {
-    slidesPerView: "auto",
-    spaceBetween: 50,
-    slidesOffsetAfter: 150,
-    watchOverflow: true,
-    freeMode: false,
-  });
-});
-
-$(function () {
   // 신간알림 더보기 버튼
 
   // 변수 선언
@@ -90,14 +68,6 @@ $(function () {
       btnAd.style.display = "none";
     });
   }
-});
-
-$(function () {
-  //서비스 소개-신간알림-리뷰 슬라이더
-  const swiper = new Swiper(".review-wrap", {
-    slidesPerView: "auto", // 자동이 아닌, 카드 width 기준으로 보여줌
-    spaceBetween: 0,
-  });
 });
 
 //동네서점지도 필터
---
+++ js/swiper.js
@@ -0,0 +1,27 @@
+$(function () {
+  const adSlider = new Swiper(".ad-slider", {
+    pagination: {
+      el: ".ad-slider .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+  });
+
+  const bestSlider = new Swiper(".best-slider", {
+    slidesPerView: "auto",
+    spaceBetween: 50,
+    slidesOffsetAfter: 150,
+    watchOverflow: true,
+    freeMode: false,
+  });
+
+  //서비스 소개-신간알림-리뷰 슬라이더
+  const swiper = new Swiper(".review-wrap", {
+    slidesPerView: "auto", // 자동이 아닌, 카드 width 기준으로 보여줌
+    spaceBetween: 0,
+  });
+});
